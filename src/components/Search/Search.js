271ec2ad8c01622cc034378998c01b9a0868c966
Patryk <jpatryk0494@gmail.com>
2021-03-07 15:27:17 +0100

    add history hook

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router-dom';
 import { setId, setQuery } from '../../actions'
 
 
 export default function Search({inputClass}) {
+    let history = useHistory();
     const dispatch = useDispatch();
     const accessKey = 'HZ9Xdpuzh8h0qelvvCRboYwsaVJ27cJYaLYvGtgyDBw'
     const query = useSelector(state=> state.query)
     const url = `https://api.unsplash.com/search/photos/?client_id=${accessKey}&query=${query}`
-    const[redirect, setRedirect] = useState(false);
+    // const[redirect, setRedirect] = useState(false);
 
     const fetchSearchPictures = async () => {
             const searchingData = await fetch(url)
@@ -27,22 +28,22 @@ export default function Search({inputClass}) {
     const handleEnterPressed = (e) => {
         if (e.key === 'Enter') {
             dispatch(setQuery(e.target.value));
-            setRedirect(true);
+            // setRedirect(true);
+            history.push('/result')
             return fetchSearchPictures();
             }
         }
 
-    const renderRedirect = () => {
-        if (redirect) {
-            setRedirect(false)
-            return <Redirect to='/result' />
-        }
-    }
+    // const renderRedirect = () => {
+    //     if (redirect) {
+    //         setRedirect(false)
+    //         return <Redirect to='/result' />
+    //     }
+    // }
 
     return (
         <div>
             <input className={inputClass} type='text' placeholder='searching phrase' onKeyDown={handleEnterPressed}></input>
-            {renderRedirect()}
         </div>
     )
 }
