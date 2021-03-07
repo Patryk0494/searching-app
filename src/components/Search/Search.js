b9b57bd5a3d0080f3876f57e69e6bde81dee8458
patryk_jankowski <74532122+Patryk0494@users.noreply.github.com>
2021-03-07 23:31:49 +0100

    fixed error with unused modules

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,21 +3,13 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { setId, setQuery } from '../../actions'
 import './Search.css'
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import CircularProgress from '@material-ui/core/CircularProgress';
-
 
 export default function Search({inputClass}) {
-    const [open, setOpen] = React.useState(false);
-    const [options, setOptions] = React.useState([]);
-    const loading = open && options.length === 0;
     let history = useHistory();
     const dispatch = useDispatch();
     const accessKey = 'HZ9Xdpuzh8h0qelvvCRboYwsaVJ27cJYaLYvGtgyDBw'
     const query = useSelector(state=> state.query)
     const url = `https://api.unsplash.com/search/photos/?client_id=${accessKey}&query=${query}`
-    const [autocompleteData, setAutocompleteData] = useState([])
 
     const fetchSearchPictures = async () => {
             const searchingData = await fetch(url)
