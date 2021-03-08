44fc926e5d74c0a512b2f23f643fd067d71b308e
Patryk <jpatryk0494@gmail.com>
2021-03-08 10:57:43 +0100

    add autocomplete template

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,21 +3,56 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { setId, setQuery } from '../../actions'
 import './Search.css'
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import {TextField} from '@material-ui/core/';
+import {Autocomplete} from '@material-ui/lab';
+import {CircularProgress} from '@material-ui/core/';
 
 
 export default function Search({inputClass}) {
-    const [open, setOpen] = React.useState(false);
-    const [options, setOptions] = React.useState([]);
-    const loading = open && options.length === 0;
+    const [open, setOpen] = useState(false);
+    const [autocompleteData, setAutocompleteData] = useState([]);
+    const loading = open && autocompleteData.length === 0;
+    
     let history = useHistory();
     const dispatch = useDispatch();
+    
     const accessKey = 'HZ9Xdpuzh8h0qelvvCRboYwsaVJ27cJYaLYvGtgyDBw'
     const query = useSelector(state=> state.query)
     const url = `https://api.unsplash.com/search/photos/?client_id=${accessKey}&query=${query}`
-    const [autocompleteData, setAutocompleteData] = useState([])
+
+    useEffect(() => {
+        let active = true;
+    
+        if (!loading) {
+            return undefined;
+        }
+    
+        (async () => {
+            const response = await fetch(`https://https://unsplash.com/nautocomplete${query}`);
+            const tags = await response.json();
+    
+            if (active) {
+                setAutocompleteData((tags).map((key) => tags[key].item[0]));
+            }
+        })();
+    
+        return () => {
+            active = false;
+        };
+        }, [loading]);
+
+        useEffect(() => {
+            if (!open) {
+                setAutocompleteData([]);
+            }
+            }, [open]);
+
+
+
+
+
+
+
 
     const fetchSearchPictures = async () => {
             const searchingData = await fetch(url)
@@ -29,30 +64,56 @@ export default function Search({inputClass}) {
     
     useEffect(()=> { 
         fetchSearchPictures();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [query])
 
-    const fetchAutocompleteData = async () => {
-        const sugestionArray = await fetch(`https://https://unsplash.com/nautocomplete${query}`)
-        .then(resp => resp.json())
-        .catch(err => console.log(err))
-
-    }
-
-    console.log(fetchSearchPictures)
-
-    const handleEnterPressed = (e) => {
-        if (e.key === 'Enter') {
-            dispatch(setQuery(e.target.value));
-            history.push('/result')
-            return fetchSearchPictures();
-            }
-        }
+    // const handleEnterPressed = (e) => {
+    //     // if (e.key === 'Enter') {
+    //         dispatch(setQuery(e.target.value));
+    //         history.push('/result')
+    //         return fetchSearchPictures();
+    //         }
+    //     // }
     
-    
-
     return (
         <div className= 'search-component'>
-            <input className={inputClass} type='text' placeholder='Search free high-resolution photos' onKeyDown={handleEnterPressed}></input>
+            {/* <input className={inputClass} type='text' placeholder='Search free high-resolution photos' onKeyDown={handleEnterPressed}></input> */}
+            <Autocomplete
+                id="asynchronous"
+                style={{ width: 300 }}
+                open={open}
+                onOpen={() => {
+                    setOpen(true);
+                }}
+                onClose={() => {
+                    setOpen(false);
+                }}
+                getOptionSelected={(autocompleteData, value) => autocompleteData.name === value.name}
+                getOptionLabel={(autocompleteData) => autocompleteData.name}
+                options={autocompleteData}
+                loading={loading}
+                includeInputInList = {true}
+                noOptionsText = 'No options'
+                onInputChange={handleEnterPressed}
+                onHighlightChange = {handleEnterPressed}
+                renderInput={(params) => (
+                    <TextField
+                    {...params}
+                    label="search"
+                    variant="outlined"
+                    classes={inputClass}
+                    InputProps={{
+                        ...params.InputProps,
+                        endAdornment: (
+                        <React.Fragment>
+                            {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                            {params.InputProps.endAdornment}
+                        </React.Fragment>
+                        ),
+                    }}
+                    />
+                )}
+                />
         </div>
     )
 }
